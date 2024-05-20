5044d7617472c4bbcd6a7cca25db82bb6778da38
Rubens Martins <rubensszm@gmail.com>
2024-05-19 21:16:31 -0300

    Componentes cabecalho e rodape adicionados

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
-cd ~/git
-mkdir memoteca
-ls -ld memoteca
+git remote add origin https://TOKEN@github.com/rubensszm/memoteca
+git clone https://github.com/rubensszm/memoteca
+cd memoteca
+#######
+sudo dnf install -y nodejs npm\n
+sudo npm install -g nvm\n
+sudo npm install -g n \n
+sudo n 14
 npm install @angular/cli@14.0.0
-npx ng new memoteca --directory ./
+#npx ng new memoteca --directory ./
+npx ng serve
+npx ng generate component home
